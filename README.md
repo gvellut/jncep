5b5466a3c87ed1ffd79104ff723765451135dd0e
Guilhem Vellut <g@vellut.com>
2020-02-01 14:58:20 +0100

    Updated doc + separated CLI & core functions in 2 files

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # jncep
 
-Simple tool to generate EPUB files for [J-Novel Club](https://j-novel.club/) pre-pub novels
+Simple command-line tool to generate EPUB files for [J-Novel Club](https://j-novel.club/) pre-pub novels
 
 # Install
 
-The tool is compatible with Python 3.6+.
+The tool requires Python 3.6+.
 
 To install, launch :
 
@@ -14,49 +14,93 @@ pip install jncep
 
 # Usage
 
-The `jncep` tool must be launched on the command-line.
+The `jncep` tool must be launched on the command-line. It has 3 commands:
 
-### Example
+- `epub`: To simply generate an EPUB file
+- `track`: To tell the tool that a series is of interest 
+- `update`: To generate EPUB files for newly updated series of interest
+
+## epub
+
+The `epub` command is used for simple EPUB generation.
+
+### Help
+
+To get some help about the arguments to the `epub` command, just launch with the --help option:
+
+```console
+~$ jncep epub --help
+Usage: jncep epub [OPTIONS] JNOVEL_CLUB_URL
+
+  Generate EPUB files for J-Novel Club pre-pub novels
+
+Options:
+  -l, --email TEXT        Login email for J-Novel Club account  [required]
+  -p, --password TEXT     Login password for J-Novel Club account  [required]
+  -o, --output DIRECTORY  Existing folder to write the output [default: The
+                          current directory]
+  -s, --parts TEXT        Specification of a range of parts to download in the
+                          form of <vol>[.part]:<vol>[.part] [default: All the
+                          content linked by the JNOVEL_CLUB_URL argument,
+                          either a single part, a whole volume or the whole
+                          series]
+  -a, --absolute          Flag to indicate that the --parts option specifies
+                          part numbers globally, instead of relative to a
+                          volume i.e. <part>:<part>
+  -v, --byvolume          Flag to indicate that the parts of different volumes
+                          shoud be output in separate EPUBs
+  -i, --images            Flag to indicate that the images of the novel should
+                          be extracted into the output folder
+  --help                  Show this message and exit.
+```
+
+### Examples
+
+#### Basic usage
+
+The following command will create a single EPUB file of part 1 of Volume 1 of the 'Tearmoon Empire' novel in the current directory:
+
+```console
+jncep epub --email "user@example.com" --password "foo%bar666!" https://j-novel.club/c/tearmoon-empire-volume-1-part-1
+```
+
+Account credentials must be passed, as well as a URL link to a part or volume or series on the J-Novel Club website. Whatever the URL links to is downloaded (single part or whole volume or whole series).
+
+The tool will then communicate with the J-Novel Club API using the specified account and download the necessary parts (texts and images), as well as a book cover.
+
+Optionnally, the JNCEP_EMAIL and JNCEP_PASSWORD env vars can be set instead of passing the --email and --password arguments when launching the tool.
+
+#### Range of parts
 
 The following command will create a single EPUB file with Parts 5 to 10 of Volume 1 of the 'Tearmoon Empire' novel (as long as the pre-pubs have not expired) in the current directory:
 
 ```console
-jncep --email "user@example.com" --password "foo%bar666!" --parts 1.5:1.10 https://j-novel.club/c/tearmoon-empire-volume-1-part-1
+jncep epub --email "user@example.com" --password "foo%bar666!" --parts 1.5:1.10 https://j-novel.club/c/tearmoon-empire-volume-1-part-1
 ```
 
-Account credentials must be passed, as well as a URL link to a part or volume or series on the J-Novel Club website. Optionnally, a range of parts / volumes can be specified, in which case the URL simply indicates the series (even if it links to a part or volume). If no range is specified, whatever the URL links to is downloaded (single part or whole volume or whole series). 
+Compared to the previous example, a range of parts / volumes has been specified, in which case the URL is simply used to indicate the series (even if it links to a part or volume).
 
-The tool will then communicate with the J-Novel Club API using the permissions of the specified account and download the necessary parts (texts and images), as well as a book cover.
+The specified range is in the shape of `<volume>[.part]:<volume>[.part]`. The specific part numbers are optional and are relative to the volume. If the part number is not specified, it is equivalent to `<volume>.1` if on the left and, if on the right, until the last part of the volume.
 
-Optionnally, the JNCEP_EMAIL and JNCEP_PASSWORD env vars can be set instead of passing the --email and --password arguments when launching the tool.
+Any of the 2 sides of the `:` range separator is optional, like `<volume>[.part]:`, which means 'every part starting with the specified part until the last in the series', or even `:`, which means 'every part in the series'. 
 
-### Help
+If the flag `--absolute` is passed, the range must of the form `<part>:<part>` where each part number refers to the part number from the beginning of the series i.e. if the first volume series has 11 parts, then `12` is the same as `2.1` without the `--absolute` flag.
 
-To get some help about the arguments, just launch with the --help option:
+## track
 
-```console
-~$ jncep --help
-Usage: jncep [OPTIONS] JNOVEL_CLUB_URL
-  Generate EPUB files for J-Novel Club pre-pub novels
+This command is used to tell the tool that it should track updates for a series.
 
-Options:
-  -l, --email TEXT     Login email for J-Novel Club account  [required]
-  -p, --password TEXT  Login password for J-Novel Club account  [required]
-  -o, --output PATH    Output EPUB file or existing folder [default: The
-                       current directory]
-  -s, --parts TEXT     Specification of a range of parts to download in the
-                       form of <vol>[.part]:<vol>[.part] [default: All the
-                       content linked by the JNOVEL_CLUB_URL argument, either
-                       a single part, a whole volume or the whole series]
-  -a, --absolute       Flag to indicate that the --parts option specifies part
-                       numbers globally, instead of relative to a volume i.e.
-                       <part>:<part>
-  --help               Show this message and exit.
-```
+TODO
 
-### Limitations
+## update
 
-This tool only works with the J-Novel novels, not manga.
+This command is used to generate EPUB files for newly updated series (previously added using the track command).
+
+TODO
+
+# Limitations
+
+This tool only works with J-Novel *novels*, not manga.
 
 # Issues
 
@@ -64,7 +108,7 @@ Report issues at https://github.com/gvellut/jncep/issues
 
 # TODO (maybe)
 
-- split EPUBs by volume
 - self-contained executable for macOS and Windows with PyInstaller
 - config file for account
 - keep track of past downloads for a series and automatically download new parts
+- Async IO
--- jncep/jncep.py
+++ jncep/jncep.py
@@ -1,19 +1,11 @@
-from html.parser import HTMLParser
 import itertools
 import os
-import re
-import time
 import traceback
 
-import attr
 import click
-from ebooklib import epub
 from termcolor import colored
 
-from . import jncapi
-
-RANGE_SEP = ":"
-
+from . import core, jncapi
 
 DEBUG = False
 
@@ -24,7 +16,7 @@ def cli():
 
 
 @cli.command(name="epub", help="Generate EPUB files for J-Novel Club pre-pub novels")
-@click.argument("url_or_slug", metavar="JNOVEL_CLUB_URL", required=True)
+@click.argument("jnc_url", metavar="JNOVEL_CLUB_URL", required=True)
 @click.option(
     "-l",
     "--email",
@@ -88,7 +80,7 @@ def cli():
     ),
 )
 def generate_epub(
-    url_or_slug,
+    jnc_url,
     email,
     password,
     part_specs=None,
@@ -97,7 +89,7 @@ def generate_epub(
     is_by_volume=False,
     is_extract_images=False,
 ):
-    slug = jncapi.slug_from_url(url_or_slug)
+    slug = jncapi.slug_from_url(jnc_url)
 
     print(f"Login with email '{email}'...")
     token = jncapi.login(email, password)
@@ -105,15 +97,15 @@ def generate_epub(
     print(f"Fetching metadata for '{slug[0]}'...")
     metadata = jncapi.fetch_metadata(token, slug)
 
-    novel = _analyze_novel_metadata(slug[1], metadata)
+    novel = core.analyze_novel_metadata(slug[1], metadata)
     if part_specs:
         print(
             f"Using part specification '{part_specs}' "
             f"(absolute={_to_yn(is_absolute)})..."
         )
-        parts_to_download = _analyze_part_specs(novel, part_specs, is_absolute)
+        parts_to_download = core.analyze_part_specs(novel, part_specs, is_absolute)
     else:
-        parts_to_download = _analyze_requested(novel)
+        parts_to_download = core.analyze_requested(novel)
 
     # preview => parts 1 of each volume, always available
     # not expired => prepub
@@ -143,516 +135,31 @@ def generate_epub(
             available_parts_to_download, lambda p: p.volume.volume_id
         ):
             parts = list(g)
-            _create_epub(token, novel, parts, output_dirpath, is_extract_images)
+            core.create_epub(token, novel, parts, output_dirpath, is_extract_images)
     else:
-        _create_epub(
+        core.create_epub(
             token, novel, available_parts_to_download, output_dirpath, is_extract_images
         )
 
 
-def _create_epub(token, novel, parts, output_dirpath, is_extract_images):
-    contents, downloaded_img_urls = _get_book_content_and_images(token, novel, parts)
-    identifier, title, author, cover_url, toc = _get_book_details(novel, parts)
-
-    if cover_url in downloaded_img_urls:
-        # no need to redownload
-        # tuple : index 0 => bytes content
-        # TODO do not add same file (same content, different name) in EPUB
-        cover_bytes = downloaded_img_urls[cover_url][0]
-    else:
-        print("Fetching cover image...")
-        cover_bytes = jncapi.fetch_image_from_cdn(cover_url)
-
-    output_filename = _to_safe_filename(title) + ".epub"
-    output_filepath = os.path.join(output_dirpath, output_filename)
-
-    if is_extract_images:
-        print("Extracting images...")
-        # TODO better name than cloudfront URL
-        for img_bytes, img_filename in downloaded_img_urls.values():
-            img_filepath = os.path.join(output_dirpath, img_filename)
-            with open(img_filepath, "wb") as img_f:
-                img_f.write(img_bytes)
-
-    _create_epub_file(
-        output_filepath,
-        identifier,
-        title,
-        author,
-        cover_bytes,
-        parts,
-        toc,
-        contents,
-        downloaded_img_urls,
-    )
-    print(colored(f"Success! EPUB generated in '{output_filepath}'!", "green"))
+@cli.command(name="track", help="Track updates to a series")
+@click.argument("url_or_slug", metavar="JNOVEL_CLUB_URL", required=True)
+def track_series(jnc_url):
+    pass
 
 
-@attr.s
-class Novel:
-    raw_serie = attr.ib()
-    raw_metadata = attr.ib()
-    requested_type = attr.ib()
-    volumes = attr.ib(default=None)
-    parts = attr.ib(default=None)
+@cli.command(name="update", help="Generate EPUB files for new parts of tracked series")
+def update_tracked():
+    pass
 
 
-@attr.s
-class Volume:
-    raw_volume = attr.ib()
-    volume_id = attr.ib()
-    parts = attr.ib(factory=list)
-
-
-@attr.s
-class Part:
-    raw_part = attr.ib()
-    num_in_volume = attr.ib()
-    volume = attr.ib()
-    content = attr.ib(default=None)
+def _to_yn(b):
+    return "yes" if b else "no"
 
 
 class NoRequestedPartAvailableError(Exception):
     def __init__(self, msg):
         super().__init__(self, msg)
-
-
-def _get_book_content_and_images(token, novel, parts_to_download):
-    downloaded_img_urls = {}
-    contents = []
-    for part in parts_to_download:
-        print(f"Fetching part '{part.raw_part.title}'...")
-        content = jncapi.fetch_content(token, part.raw_part.id)
-
-        img_urls = _img_urls(content)
-        if len(img_urls) > 0:
-            print("Fetching images found in part content...")
-            for i, img_url in enumerate(img_urls):
-                print(f"Image {i + 1}...")
-                # TODO catch, log  and ignore if error ?
-                img_bytes = jncapi.fetch_image_from_cdn(img_url)
-                # the filename relative to the epub content root
-                # file will be added to the Epub archive
-                # safe_filename on the base name (without the extension)
-                new_local_filename = _to_safe_filename(img_url[:-4]) + img_url[-4:]
-                downloaded_img_urls[img_url] = (img_bytes, new_local_filename)
-                content = content.replace(img_url, new_local_filename)
-
-        contents.append(content)
-
-    return contents, downloaded_img_urls
-
-
-def _get_book_details(novel, parts_to_download):
-    # shouldn't change between parts
-    author = novel.raw_metadata.author
-    if len(parts_to_download) == 1:
-        # single part
-        part = parts_to_download[0]
-        identifier_base = part.raw_part.titleslug
-        title = part.raw_part.title
-        # use the first part of the volume: it contains a link to
-        # a good resolution image
-        cover_url = _cover_url(part.volume.parts[0].raw_part)
-        # single part => single volume: part numbers relative to
-        # that volume
-        # TODO no TOC for single part ?
-        toc = [f"Part {part.num_in_volume}"]
-    else:
-        volume_index = set()
-        volumes = []
-        for part in parts_to_download:
-            if part.volume.volume_id in volume_index:
-                continue
-            volume_index.add(part.volume.volume_id)
-            volumes.append(part.volume)
-
-        if len(volumes) > 1:
-            volume_nums = [str(volume.raw_volume.volumeNumber) for volume in volumes]
-            volume_nums = ", ".join(volume_nums[:-1]) + " & " + volume_nums[-1]
-            title_base = f"{novel.raw_serie.title}: Volumes {volume_nums}"
-
-            part1 = (
-                f"{parts_to_download[0].volume.raw_volume.volumeNumber}."
-                f"{parts_to_download[0].num_in_volume}"
-            )
-            part2 = (
-                f"{parts_to_download[-1].volume.raw_volume.volumeNumber}."
-                f"{parts_to_download[-1].num_in_volume}"
-            )
-            part_nums = f"Parts {part1} to {part2}"
-            # TODO simplify instead ?
-            toc = [part.raw_part.title for part in parts_to_download]
-
-            # use the first part of the first volume in the requested content:
-            # first part of a volume has cvr_860.jpg which is bigger than the cover_400
-            # in volume or novel
-            cover_url = _cover_url(volumes[0].parts[0].raw_part)
-            title = f"{title_base} [{part_nums}]"
-        else:
-            volume = volumes[0]
-            title_base = volume.raw_volume.title
-            # same : use first part in volume which has cvr_860
-            cover_url = _cover_url(volume.parts[0].raw_part)
-            # relative to volume
-            toc = [f"Part {part.num_in_volume}" for part in parts_to_download]
-
-            # TODO totalPartNumber comes from the API and is set only for some unfinished
-            # volumes; If not set => maybe volume has all its parts ? if totalNumber
-            # is there, not complete for sure but some unfinished volumes do not have it
-            # either way
-            # the volumes before the last are complete for sure
-            if volume is not volumes[-1]:
-                title = f"{title_base} [Complete]"
-            else:
-                title = (
-                    f"{title_base} [Parts {parts_to_download[0].num_in_volume} to "
-                    f"{parts_to_download[-1].num_in_volume}]"
-                )
-
-        identifier_base = novel.raw_serie.titleslug
-
-    identifier = identifier_base + str(int(time.time()))
-
-    return identifier, title, author, cover_url, toc
-
-
-def _create_epub_file(
-    output_filepath,
-    identifier,
-    title,
-    author,
-    cover_bytes,
-    parts,
-    toc,
-    contents,
-    img_urls,
-):
-    lang = "en"
-    book = epub.EpubBook()
-    book.set_identifier(identifier)
-    book.set_title(title)
-    book.set_language(lang)
-    book.add_author(author)
-
-    book.set_cover("cover.jpg", cover_bytes, False)
-
-    style = """body {color: black;}
-h1 {page-break-before:always;}
-img {width: 100%; page-break-after: always;page-break-before: always;}"""
-    css = epub.EpubItem(
-        uid="style", file_name="book.css", media_type="text/css", content=style
-    )
-    book.add_item(css)
-
-    cover_page = epub.EpubHtml(title="Cover", file_name="cover.xhtml", lang=lang)
-    cover_page.content = '<img src="cover.jpg" alt="cover" />'
-    cover_page.add_item(css)
-    book.add_item(cover_page)
-
-    for img_url, img_content in img_urls.items():
-        img_bytes, local_filename = img_content
-        img = epub.EpubImage()
-        img.file_name = local_filename
-        img.media_type = "image/jpeg"
-        img.content = img_bytes
-        book.add_item(img)
-
-    chapters = []
-    for i, content in enumerate(contents):
-        c = epub.EpubHtml(title=toc[i], file_name=f"chap_{i +1}.xhtml", lang=lang)
-        c.content = content
-        c.add_item(css)
-        book.add_item(c)
-        chapters.append(c)
-
-    book.toc = chapters
-
-    book.add_item(epub.EpubNcx())
-    book.add_item(epub.EpubNav())
-
-    book.spine = [cover_page, "nav", *chapters]
-
-    epub.write_epub(output_filepath, book, {})
-
-
-def _cover_url(raw_metadata):
-    covers = list(
-        filter(
-            lambda a: "cvr" in a.filename or "cover" in a.filename,
-            raw_metadata.attachments,
-        )
-    )
-    cover = max(covers, key=lambda c: c.size)
-    return f"{jncapi.IMG_BASE_URL}{cover.fullpath}"
-
-
-class ImgUrlParser(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.img_urls = []
-
-    def handle_starttag(self, tag, tag_attrs):
-        if tag != "img":
-            return
-
-        for tag_attr in tag_attrs:
-            if tag_attr[0] == "src":
-                self.img_urls.append(tag_attr[1])
-                break
-
-
-def _img_urls(content):
-    parser = ImgUrlParser()
-    parser.feed(content)
-    return parser.img_urls
-
-
-def _to_safe_filename(name):
-    s = re.sub("[^0-9a-zA-Z]+", "_", name).strip("_")
-    return s
-
-
-def _analyze_novel_metadata(req_type, metadata):
-    """ Assumes the JSON returned from the API has all the parts and
-    that they are ordered """
-    # TODO don't trust and reorder no matter what ?
-
-    if req_type in ("PART", "VOLUME"):
-        novel = Novel(metadata.serie, metadata, req_type)
-    else:
-        novel = Novel(metadata, metadata, "NOVEL")
-
-    volume_index = {}
-    volumes = []
-    for raw_volume in novel.raw_serie.volumes:
-        volume = Volume(raw_volume, raw_volume.id)
-        volumes.append(volume)
-        volume_index[volume.volume_id] = volume
-
-    parts = []
-    for raw_part in novel.raw_serie.parts:
-        volume_id = raw_part.volumeId
-        volume = volume_index[volume_id]
-        part = Part(raw_part, len(volume.parts) + 1, volume)
-        volume.parts.append(part)
-        parts.append(part)
-
-    novel.volumes = volumes
-    novel.parts = parts
-
-    return novel
-
-
-def _analyze_requested(novel):
-    if novel.requested_type == "PART":
-        ip = novel.raw_metadata.partNumber - 1
-        return [novel.parts[ip]]
-
-    if novel.requested_type == "VOLUME":
-        iv = novel.raw_metadata.volumeNumber - 1
-        return list(novel.volumes[iv].parts)
-
-    # novel: all parts
-    return list(novel.parts)
-
-
-def _analyze_part_specs(novel, part_specs, is_absolute):
-    """ v(.p):v2(.p) or v(.p): or :v(.p) or v(.p) or : """
-
-    part_specs = part_specs.strip()
-
-    if part_specs == RANGE_SEP:
-        return novel.parts
-
-    if is_absolute:
-        return _analyze_absolute_part_specs(novel, part_specs)
-
-    return _analyze_volume_part_specs(novel, part_specs)
-
-
-def _analyze_absolute_part_specs(novel, part_specs):  # noqa: C901
-    parts = []
-    sides = part_specs.split(RANGE_SEP)
-    if len(sides) > 2:
-        raise ValueError("Multiple ':' in part specs")
-
-    reg = r"^\s*(\d+)\s*$"
-    if len(sides) == 1:
-        # not a range: single part
-        m = re.match(reg, sides[0])
-        if not m:
-            raise ValueError("Specified part must be a number")
-        fp = int(m.group(1))
-        ifp = _validate_absolute_part_number(novel, fp)
-        return [novel.parts[ifp]]
-
-    # range
-    m1 = re.match(reg, sides[0])
-    m2 = re.match(reg, sides[1])
-    if not m1 and not m2:
-        msg = "Part specification must be <number>:<number> or <number>: or :<number>"
-        raise ValueError(msg)
-
-    if m1:
-        fp = int(m1.group(1))
-        ifp = _validate_absolute_part_number(novel, fp)
-
-    if m2:
-        lp = int(m2.group(1))
-        ilp = _validate_absolute_part_number(novel, lp)
-
-    if m1 and not m2:
-        # to the end
-        for ip in range(ifp, len(novel.parts)):
-            parts.append(novel.parts[ip])
-        return parts
-
-    if m2 and not m1:
-        # since the beginning
-        # + 1 => include the second side of the range
-        for ip in range(0, ilp + 1):
-            parts.append(novel.parts[ip])
-        return parts
-
-    # both sides are present
-    if ifp > ilp:
-        msg = "Second side of the part range must be greater than first"
-        raise ValueError(msg)
-
-    # + 1 => include the second side of the range
-    for ip in range(ifp, ilp + 1):
-        parts.append(novel.parts[ip])
-    return parts
-
-
-def _validate_absolute_part_number(novel, p):
-    if p == 0:
-        raise ValueError("Specified part number must be at least 1")
-    # part specs start at 1 => transform to Python index
-    ip = p - 1
-    if ip >= len(novel.parts):
-        raise ValueError(
-            "Specified part number must be less than the number of parts in novel"
-        )
-    return ip
-
-
-def _analyze_volume_part_specs(novel, part_specs):  # noqa: C901
-    parts = []
-    sides = part_specs.split(RANGE_SEP)
-    if len(sides) > 2:
-        raise ValueError("Multiple ':' in part specs")
-
-    reg = r"^\s*(\d+)(?:\.(\d+))?\s*$"
-    if len(sides) == 1:
-        # not a range: single part
-        m = re.match(reg, sides[0])
-        if not m:
-            raise ValueError(
-                "Specification must be a of the form 'vol[.part]' (part is optional)"
-            )
-        fv = int(m.group(1))
-        if m.group(2):
-            # only the part specified
-            fp = int(m.group(2))
-            iv, ip = _validate_volume_part_number(novel, fv, fp)
-            return [novel.volumes[iv].parts[ip]]
-        else:
-            # full volume
-            iv = _validate_volume_part_number(novel, fv)
-            for part in novel.volumes[iv].parts:
-                parts.append(part)
-            return parts
-
-    # range
-    m1 = re.match(reg, sides[0])
-    m2 = re.match(reg, sides[1])
-    if not m1 and not m2:
-        msg = (
-            "Part specification must be vol[.part]:vol[.part] or vol[.part]: or "
-            ":vol[.part]"
-        )
-        raise ValueError(msg)
-
-    if m1:
-        fv = int(m1.group(1))
-        if m1.group(2):
-            fp = int(m1.group(2))
-            ifv, ifp = _validate_volume_part_number(novel, fv, fp)
-        else:
-            ifv = _validate_volume_part_number(novel, fv)
-            # beginning of the volume
-            ifp = 0
-        ifp = _to_absolute_part_number(novel, ifv, ifp)
-
-    if m2:
-        lv = int(m2.group(1))
-        if m2.group(2):
-            lp = int(m2.group(2))
-            ilv, ilp = _validate_volume_part_number(novel, lv, lp)
-        else:
-            ilv = _validate_volume_part_number(novel, lv)
-            # end of the volume
-            ilp = -1
-        # this works too if ilp == -1
-        ilp = _to_absolute_part_number(novel, ilv, ilp)
-
-    # same as for absolute part spec
-
-    if m1 and not m2:
-        # to the end
-        for ip in range(ifp, len(novel.parts)):
-            parts.append(novel.parts[ip])
-        return parts
-
-    if m2 and not m1:
-        # since the beginning
-        # + 1 => include the second side of the range
-        for ip in range(0, ilp + 1):
-            parts.append(novel.parts[ip])
-        return parts
-
-    # both sides are present
-    if ifp > ilp:
-        msg = "Second side of the vol[.part] range must be greater than first"
-        raise ValueError(msg)
-
-    # + 1 => always include the second side of the range
-    for ip in range(ifp, ilp + 1):
-        parts.append(novel.parts[ip])
-    return parts
-
-
-def _validate_volume_part_number(novel, v, p=None):
-    iv = v - 1
-
-    if iv >= len(novel.volumes):
-        raise ValueError(
-            "Specified volume number must be less than the number of volumes in novel"
-        )
-    volume = novel.volumes[iv]
-
-    if p is None:
-        return iv
-
-    ip = p - 1
-    if ip >= len(volume.parts):
-        raise ValueError(
-            "Specified part number must be less than the number of parts in volume"
-        )
-    return iv, ip
-
-
-def _to_absolute_part_number(novel, iv, ip):
-    volume = novel.volumes[iv]
-    return volume.parts[ip].raw_part.partNumber - 1
-
-
-def _to_yn(b):
-    if b:
-        return "yes"
-    return "no"
 
 
 def main():
