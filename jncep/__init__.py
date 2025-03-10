3688c5a3e2530bf81c165d87b2ed7b6950610ff9
Guilhem Vellut <g@vellut.com>
2025-03-10 19:51:35 +0100

    Fix for PyDroid

--- jncep/__init__.py
+++ jncep/__init__.py
@@ -1,1 +1,19 @@
 __version__ = "50"
+
+# fix some issue with methods not present on some system (PyDroid)
+# not actually used by jncep but import error
+import socket
+
+
+def if_nametoindex(_):
+    raise OSError("not implemented")
+
+
+def if_indextoname(_):
+    raise OSError("not implemented")
+
+
+if not hasattr(socket, "if_nametoindex"):
+    socket.if_nametoindex = if_nametoindex
+if not hasattr(socket, "if_indextoname"):
+    socket.if_indextoname = if_indextoname
