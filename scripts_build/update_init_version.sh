9c3780d1579bb3f7ca0f573f6610378831fa2a2d
Guilhem Vellut <g@vellut.com>
2025-06-10 10:45:15 +0200

    Correct sed so it works on Linux

--- scripts_build/update_init_version.sh
+++ scripts_build/update_init_version.sh
@@ -1,5 +1,5 @@
 VERSION=$(uv version --short)
 echo "Project version from uv: $VERSION"
-sed -i '' "s#^__version__ = .*#__version__ = \"$VERSION\"#" jncep/__init__.py
+sed -i "s#^__version__ = .*#__version__ = \"$VERSION\"#" jncep/__init__.py
 echo "Updated jncep/__init__.py to version $VERSION. Verifying content:"
 head -n 1 jncep/__init__.py
